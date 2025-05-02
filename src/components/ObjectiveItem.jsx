58d1f13a3cc8033c361cad41c3d4eb84ab2a051f
leovolving <leo@leoyockey.com>
2025-05-02 10:55:47 -0400

    Editing existing objective

--- src/components/ObjectiveItem.jsx
+++ src/components/ObjectiveItem.jsx
@@ -1,27 +1,87 @@
 // src/components/ObjectiveItem.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+
+import useStateToggleBoolean from '../hooks/useStateToggleBoolean';
 import useToggleObjective from '../hooks/useToggleObjective';
+
+import { useGameContext } from '../context/GameContext';
 
 const Wrapper = styled.div`
   margin: 0.5rem 0;
   display: flex;
   align-items: center;
   gap: 0.5rem;
+  flex-wrap: wrap;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
 `;
 
 const ObjectiveItem = ({ objective }) => {
   const toggleObjective = useToggleObjective();
+  const { selectedGame, setGames, setSelectedGame } = useGameContext();
+  const [isEditing, toggleIsEditing] = useStateToggleBoolean(false);
+  const [title, setTitle] = useState(objective.title);
+  const [notes, setNotes] = useState(objective.notes || '');
 
   const handleChange = () => {
     toggleObjective(objective);
   };
 
+  const handleEditSubmit = (e) => {
+    e.preventDefault();
+    updateObjective({ ...objective, title, notes });
+    toggleIsEditing();
+  };
+
+  const updateObjective = (updatedObj) => {
+    const updatedGame = {
+      ...selectedGame,
+      categories: selectedGame.categories.map((cat) =>
+        cat.id !== updatedObj.categoryId
+          ? cat
+          : {
+              ...cat,
+              objectives: cat.objectives.map((obj) =>
+                obj.id === updatedObj.id ? updatedObj : obj
+              ),
+            }
+      ),
+    };
+    setSelectedGame(updatedGame);
+    setGames((prevGames) =>
+      prevGames.map((game) => (game.id !== selectedGame.id ? game : updatedGame))
+    );
+  };
+
   return (
     <Wrapper>
       <input type="checkbox" checked={objective.completed} onChange={handleChange} />
-      <span>{objective.title}</span>
-      {objective.notes && <em> - {objective.notes}</em>}
+      {isEditing ? (
+        <Form onSubmit={handleEditSubmit}>
+          <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} required />
+          <input
+            type="text"
+            placeholder="Notes (optional)"
+            value={notes}
+            onChange={(e) => setNotes(e.target.value)}
+          />
+          <button type="submit">Save</button>
+          <button type="button" onClick={toggleIsEditing}>
+            Cancel
+          </button>
+        </Form>
+      ) : (
+        <>
+          <span>{objective.title}</span>
+          {objective.notes && <em> - {objective.notes}</em>}
+          <button onClick={toggleIsEditing}>Edit</button>
+        </>
+      )}
     </Wrapper>
   );
 };
