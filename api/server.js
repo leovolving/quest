8de8f8a9865961d03fb522b7ff851cce3168300d
leovolving <leo@leoyockey.com>
2025-08-01 11:24:17 -0400

    updated cors

--- api/server.js
+++ api/server.js
@@ -2,8 +2,16 @@ import express from 'express';
 import cors from 'cors';
 import { createClient } from '@supabase/supabase-js';
 
+console.log(process.env.ALLOWED_ORIGINS);
+
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.ALLOWED_ORIGINS.split(','),
+    methods: ['POST', 'OPTIONS'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
 app.use(express.json());
 
 console.log(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
