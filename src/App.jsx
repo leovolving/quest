630da0de0fda7cdf54a117b4ec648f016cb1beb8
leovolving <leo@leoyockey.com>
2025-08-03 11:59:57 -0400

    prevent theme toggle from interfering with other controls

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,21 @@ import GameDashboard from './components/GameDashboard';
 import GameDetail from './components/GameDetail';
 import ObjectiveDetail from './pages/ObjectiveDetail';
 
+const bufferPadding = '3rem'; // provides enough space for theme toggle
+
 const AppContainer = styled.div`
   max-width: 1200px;
   margin: 0 auto;
   padding: ${({ theme }) => theme.spacing.xl};
   width: 100%;
+  padding-bottom: ${bufferPadding};
 
   @media (max-width: 900px) {
-    padding: ${({ theme }) => theme.spacing.lg};
+    padding: ${({ theme }) => theme.spacing.lg} ${({ theme }) => theme.spacing.lg} ${bufferPadding};
     max-width: 100vw;
   }
   @media (max-width: 600px) {
-    padding: ${({ theme }) => theme.spacing.md} ${({ theme }) => theme.spacing.sm};
+    padding: ${({ theme }) => theme.spacing.md} ${({ theme }) => theme.spacing.sm} ${bufferPadding};
   }
 `;
 
