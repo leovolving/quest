6d3cbb7c827223370c4e3b835491cc2f108c3f8c
leovolving <leo@leoyockey.com>
2025-08-04 14:31:35 -0400

    fix dark mode box shadow colors

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -120,9 +120,9 @@ const darkTheme = {
     lg: '1rem',
   },
   shadows: {
-    sm: '0 1px 2px 0 rgb(0 0 0 / 0.1)',
-    md: '0 4px 6px -1px rgb(0 0 0 / 0.2)',
-    lg: '0 10px 15px -3px rgb(0 0 0 / 0.2)',
+    sm: '0 1px 2px 0 rgba(255, 255, 255, 0.05)',
+    md: '0 4px 6px -1px rgba(255, 255, 255, 0.08)',
+    lg: '0 10px 15px -3px rgba(255, 255, 255, 0.1)',
   },
   buttons: {
     primary: {
